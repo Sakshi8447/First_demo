76f04afd7937937556ee6494f4c24cb0721726d4
SakshiPathak <141705599+Sakshi8447@users.noreply.github.com>
2024-04-09 12:38:46 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("Hello World !");
+console.log("Sakshi Pathak");
+
+name = "Sakshi";
+console.log(name);
+
+age = 12;
+console.log(age);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+radius = 40;
+console.log(radius);
+
+const student = {
+    fullName : "Sakshi",
+    age : 23,
+    CGPA : 8.9,
+    isPass : true
+
+
+};
+console.log(student);
+
+const product = {
+    title : "Pen",
+    rating : 5,
+    offer : 2,
+    price : 379
+};
+console.log(product);
+
+const profile = {
+    heading : "sakshipathak711",
+    followers : 457,
+    isFollow : true,
+};
+console.log(profile);
